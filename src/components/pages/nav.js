473a49d613a9c2703a22d771de2df533bfaaac7f
ZETA <ben.r.dominguez@gmail.com>
2022-03-15 23:32:38 -0700

    updated navbar styling and added @media to styling

--- src/components/pages/nav.js
+++ src/components/pages/nav.js
@@ -8,16 +8,16 @@ export default function Nav({ currentPage, handlePageChange }) {
                 <h1 className='pageTitle'>Benjamin Dominguez</h1>  
                 <ul className='navItems'>
                     <li>
-                        <a href='#about' onClick={() => handlePageChange('About')} className='link'>About Me</a>
+                        <a href='#about' onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'link active' : 'link'}>About Me</a>
                     </li>
                     <li>
-                        <a href='#portfolio' onClick={() => handlePageChange('Portfolio')} className='link'>Portfolio</a>
+                        <a href='#portfolio' onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'link active' : 'link'}>Portfolio</a>
                     </li>
                     <li>
-                        <a href='#contact' onClick={() => handlePageChange('Contact')} className='link'>Contact Me</a>
+                        <a href='#contact' onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'link active' : 'link'}>Contact Me</a>
                     </li>
                     <li>
-                        <a href='#resume' onClick={() => handlePageChange('Resume')} className='link'>Resume</a>
+                        <a href='#resume' onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'link active' : 'link'}>Resume</a>
                     </li>
                 </ul>
                 <img src={me} alt='me' className='profilePicture'></img>
